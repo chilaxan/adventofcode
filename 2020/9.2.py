e9c31e210ab678d3b17d3ecea196d4411e1e1ece
kavandoctor1 <kavandoctor@gmail.com>
2020-12-10 21:17:20 -0800

    Added days

--- 2020/9.2.py
+++ 2020/9.2.py
@@ -4,7 +4,6 @@ s = set()
 x = 22406676
 for i in range(2,len(lines)+1):
     for j in range(len(lines)-i):
-        
         if sum(lines[j:j+i]) == x:
             print(min(lines[j:j+i])+ max(lines[j:j+i]))
 
